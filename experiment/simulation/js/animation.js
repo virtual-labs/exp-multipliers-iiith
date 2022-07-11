97f61709294f8ecdbd5892f0aa0bbd090aa71a3b
gautamxyz <gautamghai02@gmail.com>
2022-07-11 12:57:26 +0530

    code refactoring

--- experiment/simulation/js/animation.js
+++ experiment/simulation/js/animation.js
@@ -31,19 +31,19 @@ const speed = document.getElementById("speed");
 let currPos = 0;
 
 const objects = [document.getElementById("a1"), document.getElementById("a0"), document.getElementById("b1"), document.getElementById("b0"), document.getElementById("a1"), document.getElementById("a0"), document.getElementById("b1"), document.getElementById("b0"), document.getElementById("c3"), document.getElementById("c2"), document.getElementById("c1"), document.getElementById("c0")];
-const textinput = [
+const textInput = [
     document.createElementNS(svgns, "text"), 
     document.createElementNS(svgns, "text"), 
     document.createElementNS(svgns, "text"), 
     document.createElementNS(svgns, "text")
 ];
-const textoutput = [
+const textOutput = [
     document.createElementNS(svgns, "text"), 
     document.createElementNS(svgns, "text"), 
     document.createElementNS(svgns, "text"), 
     document.createElementNS(svgns, "text")
 ];
-const inputdots = [
+const inputDots = [
     document.createElementNS(svgns, "circle"),
     document.createElementNS(svgns, "circle"),
     document.createElementNS(svgns, "circle"),
@@ -67,149 +67,149 @@ function demoWidth() {
 
 //initialise input text
 function textIOInit() {
-    for (const text of textinput) {
+    for (const text of textInput) {
         text.textContent = 2;
     }
 }
 
 function outputCoordinates() {
     let xcor = 695;
-    setCoordinates(xcor, 210, textoutput[0]);
-    svg.append(textoutput[0]);
-    setCoordinates(xcor, 345, textoutput[1]);
-    svg.append(textoutput[1]);
-    setCoordinates(xcor, 465, textoutput[2]);
-    svg.append(textoutput[2]);
-    setCoordinates(xcor, 545, textoutput[3]);
-    svg.append(textoutput[3]);
+    setCoordinates(xcor, 210, textOutput[0]);
+    svg.append(textOutput[0]);
+    setCoordinates(xcor, 345, textOutput[1]);
+    svg.append(textOutput[1]);
+    setCoordinates(xcor, 465, textOutput[2]);
+    svg.append(textOutput[2]);
+    setCoordinates(xcor, 545, textOutput[3]);
+    svg.append(textOutput[3]);
 }
 
 
 function initInputDots() {
     //sets the coordinates of the input dots
-    fillInputDots(inputdots[0], 20, 220, 15, "#FF0000");
-    svg.append(inputdots[0]);
-    fillInputDots(inputdots[1], 20, 560, 15, "#FF0000");
-    svg.append(inputdots[1]);
-    fillInputDots(inputdots[2], 20, 340, 15, "#FF0000");
-    svg.append(inputdots[2]);
-    fillInputDots(inputdots[3], 20, 440, 15, "#FF0000");
-    svg.append(inputdots[3]);
-    fillInputDots(inputdots[4], 20, 220, 15, "#FF0000");
-    svg.append(inputdots[4]);
-    fillInputDots(inputdots[5], 20, 560, 15, "#FF0000");
-    svg.append(inputdots[5]);
-    fillInputDots(inputdots[6], 20, 340, 15, "#FF0000");
-    svg.append(inputdots[6]);
-    fillInputDots(inputdots[7], 20, 440, 15, "#FF0000");
-    svg.append(inputdots[7]);
+    fillInputDots(inputDots[0], 20, 220, 15, "#FF0000");
+    svg.append(inputDots[0]);
+    fillInputDots(inputDots[1], 20, 560, 15, "#FF0000");
+    svg.append(inputDots[1]);
+    fillInputDots(inputDots[2], 20, 340, 15, "#FF0000");
+    svg.append(inputDots[2]);
+    fillInputDots(inputDots[3], 20, 440, 15, "#FF0000");
+    svg.append(inputDots[3]);
+    fillInputDots(inputDots[4], 20, 220, 15, "#FF0000");
+    svg.append(inputDots[4]);
+    fillInputDots(inputDots[5], 20, 560, 15, "#FF0000");
+    svg.append(inputDots[5]);
+    fillInputDots(inputDots[6], 20, 340, 15, "#FF0000");
+    svg.append(inputDots[6]);
+    fillInputDots(inputDots[7], 20, 440, 15, "#FF0000");
+    svg.append(inputDots[7]);
 }
 function dotsDisappear() {
-    for (const inputDot of inputdots) {
+    for (const inputDot of inputDots) {
         objectDisappear(inputDot);
     }
 }
 function dotsAppear() {
-    for (const inputDot of inputdots) {
+    for (const inputDot of inputDots) {
         objectAppear(inputDot);
     }
 }
 
 function calculateAnd() {
     if (currPos === 0) {
-        if (textinput[0].textContent === "0" || textinput[2].textContent === "0") {
-            setter("0", inputdots[0]);
-            setter("0", inputdots[2]);
+        if (textInput[0].textContent === "0" || textInput[2].textContent === "0") {
+            setter("0", inputDots[0]);
+            setter("0", inputDots[2]);
         }
-        if (textinput[0].textContent === "0" || textinput[3].textContent === "0") {
-            setter("0", inputdots[4]);
-            setter("0", inputdots[3]);
+        if (textInput[0].textContent === "0" || textInput[3].textContent === "0") {
+            setter("0", inputDots[4]);
+            setter("0", inputDots[3]);
         }
-        if (textinput[1].textContent === "0" || textinput[2].textContent === "0") {
-            setter("0", inputdots[5]);
-            setter("0", inputdots[6]);
+        if (textInput[1].textContent === "0" || textInput[2].textContent === "0") {
+            setter("0", inputDots[5]);
+            setter("0", inputDots[6]);
         }
-        if (textinput[1].textContent === "0" || textinput[3].textContent === "0") {
-            setter("0", inputdots[7]);
-            setter("0", inputdots[1]);
+        if (textInput[1].textContent === "0" || textInput[3].textContent === "0") {
+            setter("0", inputDots[7]);
+            setter("0", inputDots[1]);
         }
         currPos = 1;
-        objectDisappear(inputdots[7]);
+        objectDisappear(inputDots[7]);
     }
     else if (currPos === 1) {
-        if (String(inputdots[4].style.fill) === ZERO || String(inputdots[6].style.fill) === ZERO) {
-            setter("0", inputdots[4]);
-            setter("0", inputdots[6]);
+        if (String(inputDots[4].style.fill) === ZERO || String(inputDots[6].style.fill) === ZERO) {
+            setter("0", inputDots[4]);
+            setter("0", inputDots[6]);
         }
         else {
-            setter("1", inputdots[4]);
-            setter("1", inputdots[6]);
+            setter("1", inputDots[4]);
+            setter("1", inputDots[6]);
         }
         currPos++;
-        objectDisappear(inputdots[5]);
+        objectDisappear(inputDots[5]);
     }
     else if (currPos === 2) {
-        if (String(inputdots[4].style.fill) === ZERO || inputdots[0].style.fill === ZERO) {
+        if (String(inputDots[4].style.fill) === ZERO || inputDots[0].style.fill === ZERO) {
 
-            setter("0", inputdots[4]);
-            setter("0", inputdots[0]);
+            setter("0", inputDots[4]);
+            setter("0", inputDots[0]);
         }
-        objectDisappear(inputdots[6]);
-        objectDisappear(inputdots[4]);
+        objectDisappear(inputDots[6]);
+        objectDisappear(inputDots[4]);
     }
 
 }
 
 function calculateXOR() {
     if (currPos === 1) {
-        if (inputdots[3].style.fill !== inputdots[5].style.fill) {
-            setter("1", inputdots[3]);
-            setter("1", inputdots[5]);
+        if (inputDots[3].style.fill !== inputDots[5].style.fill) {
+            setter("1", inputDots[3]);
+            setter("1", inputDots[5]);
         }
         else {
-            setter("0", inputdots[3]);
-            setter("0", inputdots[5]);
+            setter("0", inputDots[3]);
+            setter("0", inputDots[5]);
         }
 
     }
     if (currPos === 2) {
-        if (inputdots[6].style.fill !== inputdots[2].style.fill) {
+        if (inputDots[6].style.fill !== inputDots[2].style.fill) {
 
-            setter("1", inputdots[6]);
-            setter("1", inputdots[2]);
+            setter("1", inputDots[6]);
+            setter("1", inputDots[2]);
         }
         else {
-            setter("0", inputdots[6]);
-            setter("0", inputdots[2]);
+            setter("0", inputDots[6]);
+            setter("0", inputDots[2]);
         }
 
     }
 }
 function inputDotDisappear() {
-    for (const inputDot of inputdots) {
+    for (const inputDot of inputDots) {
         fillColor(inputDot, "#008000");
     }
 }
 
 // function to disappear the output text
 function outputDisappear() {
-    for (const text of textoutput) {
+    for (const text of textOutput) {
         objectDisappear(text);
     }
 }
 // function to appear the output text
 function outputVisible() {
-    for (const text of textoutput) {
+    for (const text of textOutput) {
         objectAppear(text);
     }
 }
 function inputTextDisappear() {
-    for (const text of textinput) {
+    for (const text of textInput) {
         objectDisappear(text);
     }
 }
 function inputTextAppear() {
-    for (const text of textinput) {
+    for (const text of textInput) {
         if (text.textContent !== "2")
             objectAppear(text);
     }
@@ -229,73 +229,73 @@ function allDisappear() {
 }
 function outputSetter() {
 
-    for (let index = 0; index < textoutput.length; index++) {
-        setter(textoutput[index].textContent, objects[index + 8]);
+    for (let index = 0; index < textOutput.length; index++) {
+        setter(textOutput[index].textContent, objects[index + 8]);
     }
 
 }
 function outputHandler() {
-    let a1 = parseInt(textinput[0].textContent);
-    let a0 = parseInt(textinput[1].textContent);
-    let b1 = parseInt(textinput[2].textContent);
-    let b0 = parseInt(textinput[3].textContent);
+    let a1 = parseInt(textInput[0].textContent);
+    let a0 = parseInt(textInput[1].textContent);
+    let b1 = parseInt(textInput[2].textContent);
+    let b0 = parseInt(textInput[3].textContent);
     let num1 = (a1 * 2) + a0;
     let num2 = (b1 * 2) + b0;
     let product = num1 * num2;
     let ans = product.toString(2).padStart(4, "0");
-    for (let i = 0; i < textoutput.length; i++) {
-        textoutput[i].textContent = (ans[i]);
+    for (let i = 0; i < textOutput.length; i++) {
+        textOutput[i].textContent = (ans[i]);
     }
 
 }
 function appendInput1() {
-    if (textinput[0].textContent !== "0" && timeline.progress() === 0) {
+    if (textInput[0].textContent !== "0" && timeline.progress() === 0) {
         changeTo0(15, 225, 0, 0);
     }
-    else if (textinput[0].textContent !== "1" && timeline.progress() === 0) {
+    else if (textInput[0].textContent !== "1" && timeline.progress() === 0) {
         changeTo1(15, 225, 0, 0);
     }
-    setter(textinput[0].textContent, inputdots[0]);
-    setter(textinput[0].textContent, inputdots[4]);
+    setter(textInput[0].textContent, inputDots[0]);
+    setter(textInput[0].textContent, inputDots[4]);
 }
 
 
 function appendInput2() {
-    if (textinput[1].textContent !== "0" && timeline.progress() === 0) {
+    if (textInput[1].textContent !== "0" && timeline.progress() === 0) {
         changeTo0(15, 565, 1, 1);
     }
-    else if (textinput[1].textContent !== "1" && timeline.progress() === 0) {
+    else if (textInput[1].textContent !== "1" && timeline.progress() === 0) {
         changeTo1(15, 565, 1, 1);
     }
-    setter(textinput[1].textContent, inputdots[1]);
-    setter(textinput[1].textContent, inputdots[5]);
+    setter(textInput[1].textContent, inputDots[1]);
+    setter(textInput[1].textContent, inputDots[5]);
 }
 function appendInput3() {
-    if (textinput[2].textContent !== "0" && timeline.progress() === 0) {
+    if (textInput[2].textContent !== "0" && timeline.progress() === 0) {
         changeTo0(15, 345, 2, 2);
     }
-    else if (textinput[1].textContent !== "1" && timeline.progress() === 0) {
+    else if (textInput[1].textContent !== "1" && timeline.progress() === 0) {
         changeTo1(15, 345, 2, 2);
     }
-    setter(textinput[2].textContent, inputdots[2]);
-    setter(textinput[2].textContent, inputdots[6]);
+    setter(textInput[2].textContent, inputDots[2]);
+    setter(textInput[2].textContent, inputDots[6]);
 }
 function appendInput4() {
-    if (textinput[3].textContent !== "0" && timeline.progress() === 0) {
+    if (textInput[3].textContent !== "0" && timeline.progress() === 0) {
         changeTo0(15, 445, 3, 3);
     }
-    else if (textinput[3].textContent !== "1" && timeline.progress() === 0) {
+    else if (textInput[3].textContent !== "1" && timeline.progress() === 0) {
         changeTo1(15, 445, 3, 3);
     }
-    setter(textinput[3].textContent, inputdots[3]);
-    setter(textinput[3].textContent, inputdots[7]);
+    setter(textInput[3].textContent, inputDots[3]);
+    setter(textInput[3].textContent, inputDots[7]);
 }
 
 function changeTo1(coordinateX, coordinateY, object, textObject) {
 
-    textinput[textObject].textContent = 1;
-    svg.appendChild(textinput[textObject]);
-    setCoordinates(coordinateX, coordinateY, textinput[textObject]);
+    textInput[textObject].textContent = 1;
+    svg.appendChild(textInput[textObject]);
+    setCoordinates(coordinateX, coordinateY, textInput[textObject]);
 
     fillColor(objects[object], "#29e");
     fillColor(objects[object + 4], "#29e");
@@ -305,9 +305,9 @@ function changeTo1(coordinateX, coordinateY, object, textObject) {
 
 function changeTo0(coordinateX, coordinateY, object, textObject) {
 
-    textinput[textObject].textContent = 0;
-    svg.appendChild(textinput[textObject]);
-    setCoordinates(coordinateX, coordinateY, textinput[textObject]);
+    textInput[textObject].textContent = 0;
+    svg.appendChild(textInput[textObject]);
+    setCoordinates(coordinateX, coordinateY, textInput[textObject]);
 
     fillColor(objects[object], "#eeeb22");
     fillColor(objects[object + 4], "#eeeb22");
@@ -319,7 +319,7 @@ function reboot() {
     for (const elements of objects) {
         elements.style.fill = "#008000";
     }
-    for (const text of textinput) {
+    for (const text of textInput) {
         text.textContent = 2;
     }
 
@@ -383,7 +383,7 @@ function stopCircuit() {
     }
 }
 function startCircuit() {
-    if (textinput[0].textContent !== "2" && textinput[1].textContent !== "2" && textinput[2].textContent !== "2" && textinput[3].textContent !== "2") {
+    if (textInput[0].textContent !== "2" && textInput[1].textContent !== "2" && textInput[2].textContent !== "2" && textInput[3].textContent !== "2") {
         if (!circuitStarted) {
             circuitStarted = true;
         }
@@ -397,22 +397,22 @@ function startCircuit() {
             observ.innerHTML = "Please Restart the simulation";
         }
     }
-    else if (textinput[0].textContent === "2") {
+    else if (textInput[0].textContent === "2") {
         observ.innerHTML = "Please set the value of A1 to 0 or 1";
     }
-    else if (textinput[1].textContent === "2") {
+    else if (textInput[1].textContent === "2") {
         observ.innerHTML = "Please set the value of A0 to 0 or 1";
     }
-    else if (textinput[2].textContent === "2") {
+    else if (textInput[2].textContent === "2") {
         observ.innerHTML = "Please set the value of B1 to 0 or 1";
     }
-    else if (textinput[3].textContent === "2") {
+    else if (textInput[3].textContent === "2") {
         observ.innerHTML = "Please set the value of B0 to 0 or 1";
     }
 }
 
 function simulator() {
-    timeline.to(inputdots[0], {
+    timeline.to(inputDots[0], {
         motionPath: {
             path: "#path1",
             align: "#path1",
@@ -428,7 +428,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[0], {
+    timeline.to(inputDots[0], {
         motionPath: {
             path: "#path13",
             align: "#path13",
@@ -445,7 +445,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[0], {
+    timeline.to(inputDots[0], {
         motionPath: {
             path: "#path16",
             align: "#path16",
@@ -462,7 +462,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[4], {
+    timeline.to(inputDots[4], {
         motionPath: {
             path: "#path2",
             align: "#path2",
@@ -478,7 +478,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[4], {
+    timeline.to(inputDots[4], {
         motionPath: {
             path: "#path9",
             align: "#path9",
@@ -495,7 +495,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[4], {
+    timeline.to(inputDots[4], {
         motionPath: {
             path: "#path20",
             align: "#path20",
@@ -512,7 +512,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[1], {
+    timeline.to(inputDots[1], {
         motionPath: {
             path: "#path7",
             align: "#path7",
@@ -528,7 +528,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[1], {
+    timeline.to(inputDots[1], {
         motionPath: {
             path: "#path19",
             align: "#path19",
@@ -545,7 +545,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[5], {
+    timeline.to(inputDots[5], {
         motionPath: {
             path: "#path8",
             align: "#path8",
@@ -561,7 +561,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[5], {
+    timeline.to(inputDots[5], {
         motionPath: {
             path: "#path11",
             align: "#path11",
@@ -578,7 +578,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[2], {
+    timeline.to(inputDots[2], {
         motionPath: {
             path: "#path3",
             align: "#path3",
@@ -594,7 +594,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[2], {
+    timeline.to(inputDots[2], {
         motionPath: {
             path: "#path14",
             align: "#path14",
@@ -611,7 +611,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[2], {
+    timeline.to(inputDots[2], {
         motionPath: {
             path: "#path17",
             align: "#path17",
@@ -628,7 +628,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[6], {
+    timeline.to(inputDots[6], {
         motionPath: {
             path: "#path4",
             align: "#path4",
@@ -644,7 +644,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[6], {
+    timeline.to(inputDots[6], {
         motionPath: {
             path: "#path12",
             align: "#path12",
@@ -661,7 +661,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[6], {
+    timeline.to(inputDots[6], {
         motionPath: {
             path: "#path15",
             align: "#path15",
@@ -679,7 +679,7 @@ function simulator() {
 
     }, 0);
 
-    timeline.to(inputdots[3], {
+    timeline.to(inputDots[3], {
         motionPath: {
             path: "#path5",
             align: "#path5",
@@ -695,7 +695,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[3], {
+    timeline.to(inputDots[3], {
         motionPath: {
             path: "#path10",
             align: "#path10",
@@ -712,7 +712,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[3], {
+    timeline.to(inputDots[3], {
         motionPath: {
             path: "#path18",
             align: "#path18",
@@ -729,7 +729,7 @@ function simulator() {
         paused: false,
 
     }, 0);
-    timeline.to(inputdots[7], {
+    timeline.to(inputDots[7], {
         motionPath: {
             path: "#path6",
             align: "#path6",
